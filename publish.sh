f95031bf87cf5ea71dcb61ac08341220beea2bfb
Eric Fay <eric.daniel.fay@gmail.com>
2019-07-02 13:06:31 -0400

    publish as standalone

--- publish.sh
+++ publish.sh
@@ -8,5 +8,5 @@ writer['txt']='plain'
 
 for key in ${!writer[@]}; do
     output_file="pub/Eric_Fay_Resume.${key}"
-    pandoc ${markdown_source_file} --to=${writer[${key}]} --output=${output_file}
+    pandoc --standalone ${markdown_source_file} --to=${writer[${key}]} --output=${output_file}
 done
